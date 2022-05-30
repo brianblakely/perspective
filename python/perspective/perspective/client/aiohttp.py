048ee9385f79fee933d4d16616385cf7717275c8
Tim Paine <t.paine154@gmail.com>
2022-05-30 15:41:31 -0400

    Asyncify the perspective client, abstract away the websocket handler into its own class and utility classes

--- python/perspective/perspective/client/aiohttp.py
+++ python/perspective/perspective/client/aiohttp.py
@@ -1,12 +1,7 @@
 ################################################################################
 #
-# Copyright (c) 2019, the Perspective Authors.
+# Copyright (c) 2022, the Perspective Authors.
 #
 # This file is part of the Perspective library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
-
-from .tornado_handler import PerspectiveTornadoHandler
-from ..client.tornado import websocket
-
-__all__ = ["PerspectiveTornadoHandler", "websocket"]
